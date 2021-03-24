5d9587674e95c7144f0fb977bdb39554c4c09fcd
92an <micke92an@outlook.com>
2021-03-24 23:43:30 +0000

    categories filer implemented

--- boutique_ado/products/views.py
+++ boutique_ado/products/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse ,get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 # Create your views here.
 
+
+# the view for all products
 def all_products(request):
 
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if "category" in request.GET:
+            categories = request.GET["category"].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if "q" in request.GET:
+            query = request.GET["q"]
+            if not query:
+                messages.error(request, "You have to enter search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        "products":products,
+        "products": products,
+        'search_term': query,
+        "current_category": categories,
     }
 
     return render(request, "products/products.html", context)
+
+
+# View fo product details when inspecting a specific product
+def product_detail(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "products/product_detail.html", context)
